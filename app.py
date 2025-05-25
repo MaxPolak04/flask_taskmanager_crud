bb9a1b0f366142e8b75f55ab65d8921e36f498fe
Maksymilian Polak <max.polak04@gmail.com>
2025-05-25 23:33:39 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, url_for
+
+
+app = Flask(__name__)
+# db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    elif request.method == 'POST':
+        pass
+
+
+if __name__ == '__main__':
+    app.run()
