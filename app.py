45e422ace216332876ea46bff6820e27b10fa02b
Maksymilian Polak <max.polak04@gmail.com>
2025-05-27 13:45:01 +0200

    add create task route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from config import Config
 from models import Todo, db
 from pathlib import Path
@@ -24,10 +24,23 @@ def form():
         return render_template('response.html', email=email, message=message)
     
 
-@app.route('/task', methods=['GET'])
-def get_all_task():
+@app.route('/tasks', methods=['GET'])
+def get_all_tasks():
     tasks = Todo.query.all()
     return render_template('task.html', tasks=tasks)
+
+
+@app.route('/create-task', methods=['GET', 'POST'])
+def create_task():
+    if request.method == 'GET':
+        return render_template('create_task.html')
+    elif request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        new_task = Todo(title=title, description=description)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('get_all_tasks'))
     
 
 if __name__ == '__main__':
