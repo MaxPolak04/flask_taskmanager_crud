abe4beb9434739bf8588706954f96f7d04c51f3c
Maksymilian Polak <max.polak04@gmail.com>
2025-05-27 20:08:53 +0200

    add update and delete features

--- app.py
+++ app.py
@@ -33,15 +33,34 @@ def get_all_tasks():
 @app.route('/create-task', methods=['GET', 'POST'])
 def create_task():
     if request.method == 'GET':
-        return render_template('create_task.html')
+        return render_template('task-form.html')
     elif request.method == 'POST':
         title = request.form.get('title')
         description = request.form.get('description')
+        print(request.form)
+        print(title, description)
         new_task = Todo(title=title, description=description)
         db.session.add(new_task)
         db.session.commit()
         return redirect(url_for('get_all_tasks'))
     
+
+@app.route('/update-task/<int:task_id>', methods=['POST'])
+def update_task(task_id):
+    task = Todo.query.get_or_404(task_id)
+    task.title = request.form.get('title')
+    task.description = request.form.get('description')
+    task.is_done = True if request.form.get('is_done') == 'True' else False
+    db.session.commit()
+    return redirect(url_for('get_all_tasks'))
+
+
+@app.route('/delete-task/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Todo.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('get_all_tasks'))
 
 if __name__ == '__main__':
     app.run()
